b1535c0cdaa0c6f999838f971ec4982ab4175501
Messier43 <74980186+Messier43@users.noreply.github.com>
2023-04-04 22:34:05 +0200

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -9,15 +9,25 @@ sbox = [0xc,5,6,0xb,9,0,0xa,0xd,0x3,0xe,0xf,8,4,7,1,2]
 #cle : clé (entier)
 #Renvoie le chiffré (entier)
 def chiffrement(etat, cle):
+	"""
+	Entrée : message à chiffrer (int) et clé (int)
+	Sortie : PRESENT24(cle)(message)
+	Le chiffré est produit en cadençant la clé
+	Ensuite pour chaqur tour, les bits du message sont substitués puis 
+	permutés et XORé à la sous-clé du tour correspondant
+	"""
 	k = cadencement(cle)
 	for i in k:
 		etat = permutation(substitution(etat, sbox))^i
 	return etat
 
 
-#value : suite de bits (entier) de 24 bits à permuter
-#Renvoie l'entier dont les bits ont été permutés
+
 def permutation(value):
+	"""
+	value : suite de bits (entier) de 24 bits à permuter
+	Renvoie l'entier dont les bits ont été permutés
+	"""
 	value = permutation_step(value, 0x00001100, 16)
 	value = permutation_step(value, 0x000c080c, 4)
 	value = permutation_step(value, 0x00220022, 2)
@@ -34,9 +44,12 @@ def permutation_step(value, mask, shift):
 	return (value ^ result) ^ (result << shift)
 
 
-#entree : suite de 24 bits (entier) à substituer
-#Renvoie l'entier substitué
+
 def substitution(entree, liste_substitution):
+	"""
+	entree : suite de 24 bits (entier) à substituer
+	Renvoie l'entier substitué
+	"""
 	sortie = 0
 	mask = 0xf
 	for i in [0, 4, 8, 12, 16, 20]:
@@ -46,14 +59,11 @@ def substitution(entree, liste_substitution):
 	return sortie
 
 
-#Rotation gauche d'un entier 'nombre' de taille 'taille' sur 'decalage' bits
-def rotation_gauche(nombre, decalage, taille):
-	mask_taille_limite = (1 << taille) -1
-	return ((nombre << decalage) & mask_taille_limite) | (nombre >> (taille - decalage))
 
-
-#Renvoie une liste contenant les 11 sous-cĺés à partir de la clé 'maitre'
 def cadencement(K):
+	"""
+	Renvoie une liste contenant les 11 sous-cĺés à partir de la clé 'maitre'
+	"""
 	resultat = []
 	K <<= 56
 	for i in range(1,11):
@@ -63,65 +73,6 @@ def cadencement(K):
 		resultat.append(K>>16 & 0xffffff)
 	#resultat.append(K>>16 & 0xffffff) #derniere sous clé
 	return resultat
-
-
-
-
-###NON UTILISÉ###
-'''
-#liste de permutation du chiffrement du PRESENT24
-#liste[i] => nouvelle position du bit i
-liste_permutation = [0,6,12,18,1,7,13,19,2,8,14,20,3,9,15,21,4,10,16,22,5,11,17,23]
-
-def permutation3(entree):
-	sortie = 0
-	bit_sortie=0
-	for i in range(4): #colonne
-		for i4 in range(i,24,4):
-			# += plus rapide que |= ^=
-			sortie += (((1<<i4) & entree ) and (1<< (bit_sortie)))
-			bit_sortie += 1
-	return sortie
-
-def generation_sbox_large():
-	sortie = []
-	for k in range(6):
-		sortie.append([(x<<k) for x in sbox])
-	return sortie
-
-sbox_large = generation_sbox_large()
-
-def permutation_old(entree, permutations):
-	sortie = 0
-	mask = 1 << 23
-	for i in range(24):
-		#Pour chaque bit entrée à 1, on ajoute 1 à la position indiquée dans la liste
-		sortie += (mask & entree) and (1 << 23-permutations[i])
-		mask >>= 1
-	return sortie
-'''
-################
-
-
-
-
-########## TEST #########
-def tests_chiffrement(num_test):
-	match num_test:
-		case 0: #Substitution
-			print(hex(substitution(0x30000f,sbox)))
-		case 1: #Permutation
-			print(bin(permutation(0x888888)))
-		case 2: #Rotation Gauche
-			print(bin(rotation_gauche(0b111010000,3,9)))
-		case 3:
-			res = cadencement(0)
-			print([hex(x) for x in res])
-		case 4:
-			print(hex(chiffrement(0xf955b9,0xd1bd2d)))
-#tests_chiffrement(int(argv[1]))
-
-
 
 ########## CLI #########
 
